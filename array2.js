fff6a1301e9d542f383d24cd2f0fed3bc695f9b6
maoma1 <jenniensofor@gmail.com>
2021-12-19 22:09:12 +0000

    first proper commit of array 2

---
+++ array2.js
@@ -0,0 +1,16 @@
+/*Define a function that takes in an array of elements and joins them together into a string separated by a comma with a space after the comma.
+
+Example array: let colors = ["Red", "Green", "White", "Black"];
+
+Should look like: "Red, Green, White, Black"
+
+Do NOT use the "toString" method.
+*/
+
+//using the .join() method, create a function that joins components of an array to form a string
+
+const myArray = ['red', 'Green', 'white', 'black'];
+
+let newString = myArray.join();
+
+console.log(newString)
