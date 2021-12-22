b4bc9abb8e5730ba334c4824a3b73ebeaf5b121b
maoma1 <jenniensofor@gmail.com>
2021-12-22 11:24:14 +0000

    function added

--- array2.js
+++ array2.js
@@ -9,8 +9,17 @@ Do NOT use the "toString" method.
 
 //using the .join() method, create a function that joins components of an array to form a string
 
+
+//Define a function that takes in an array of elements and joins them together into a string separated by a comma with a space after the comma.
+
+
+
 const myArray = ['red', 'Green', 'white', 'black'];
 
-let newString = myArray.join();
+function joinElements(a) {
 
-console.log(newString)
+    let result = a.join();
+    return result
+}
+
+console.log(joinElements(myArray));
